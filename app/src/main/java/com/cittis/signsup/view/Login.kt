bbca8761cedaa08b4de809db47742a1c0b6d1ca2
Mavsters <mtorres@cittus.com>
2019-04-18 09:05:39 -0500

    [Add] Login - Firebase

--- app/src/main/java/com/cittis/signsup/view/Login.kt
+++ app/src/main/java/com/cittis/signsup/view/Login.kt
@@ -1,14 +1,22 @@
 package com.cittis.signsup.view
 
+import android.app.ProgressDialog
+import android.content.Context
 import android.os.Bundle
+import android.support.annotation.VisibleForTesting
 import android.support.v4.app.Fragment
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import com.cittis.signsup.R
 import com.cittis.signsup.connection.DataBase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 /**
  * A simple [Fragment] subclass.
@@ -26,7 +34,13 @@ class Login : Fragment() {
     private lateinit var connection: DataBase
     // Make Bundle
     val bundle = Bundle()
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
 
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -42,21 +56,239 @@ class Login : Fragment() {
     }
 
     private fun initProcess() {
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // [END initialize_auth]
 
-        // Data Base
-        var connection: DataBase = DataBase(viewMain.context, this)
-        connection.loginCall()
-        viewMain.findViewById<Button>(R.id.btn_login).setOnClickListener {
-            // Call the API
+        // Actions
+        setCreateAccount()
+        setSignIn()
+        setSignOut()
+        setSendEmailVerification()
 
-            //connection.postApiCall()
-            connection.loginCall()
-            Log.e("Data", connection.resultTextView.toString())
-            Log.e("Data", connection.resultTextView.toString())
-            Log.e("Data", connection.resultTextView.toString())
-            Log.e("Data", connection.resultTextView.toString())
+    }
+
+    private fun setSendEmailVerification() {
+        viewMain.findViewById<Button>(R.id.verifyEmailButton).setOnClickListener {
+            sendEmailVerification()
+        }
+    }
+
+    private fun sendEmailVerification() {
+        // Disable button
+        viewMain.findViewById<Button>(R.id.verifyEmailButton).isEnabled = false
+
+        // Send verification email
+        // [START send_email_verification]
+        val user = auth.currentUser
+        user?.sendEmailVerification()?.addOnCompleteListener { task ->
+            // [START_EXCLUDE]
+            // Re-enable button
+            viewMain.findViewById<Button>(R.id.verifyEmailButton).isEnabled = true
+
+            if (task.isSuccessful) {
+                Toast.makeText(
+                    viewMain.context,
+                    "Verification email sent to ${user.email} ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Log.e(TAG, "sendEmailVerification", task.exception)
+                Toast.makeText(
+                    viewMain.context,
+                    "Failed to send verification email.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            // [END_EXCLUDE]
+        }
+        // [END send_email_verification]
+    }
+
+    private fun setSignOut() {
+        viewMain.findViewById<Button>(R.id.signOutButton).setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        updateUI(null)
+    }
+
+    private fun setCreateAccount() {
+        viewMain.findViewById<Button>(R.id.emailCreateAccountButton).setOnClickListener {
+            var email = viewMain.findViewById<EditText>(R.id.fieldEmail).text.toString()
+            var password = viewMain.findViewById<EditText>(R.id.fieldPassword).text.toString()
+            createAccount(email, password)
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressDialog()
+
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            // .addOnCompleteListener(viewMain.context) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "createUserWithEmail:success")
+                val user = auth.currentUser
+                updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                Toast.makeText(
+                    viewMain.context, "Authentication failed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                updateUI(null)
+            }
+
+            // [START_EXCLUDE]
+            hideProgressDialog()
+            // [END_EXCLUDE]
+        }
+        // [END create_user_with_email]
+    }
+
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = viewMain.findViewById<EditText>(R.id.fieldEmail).text.toString()
+        if (TextUtils.isEmpty(email)) {
+            viewMain.findViewById<EditText>(R.id.fieldEmail).error = "Required."
+            valid = false
+        } else {
+            viewMain.findViewById<EditText>(R.id.fieldEmail).error = null
+        }
+
+        val password = viewMain.findViewById<EditText>(R.id.fieldPassword).text.toString()
+        if (TextUtils.isEmpty(password)) {
+            viewMain.findViewById<EditText>(R.id.fieldPassword).error = "Required."
+            valid = false
+        } else {
+            viewMain.findViewById<EditText>(R.id.fieldPassword).error = null
+        }
+
+        return valid
+    }
+
+    private fun setSignIn() {
+
+        viewMain.findViewById<Button>(R.id.emailSignInButton).setOnClickListener {
+            var email = viewMain.findViewById<EditText>(R.id.fieldEmail).text.toString()
+            var password = viewMain.findViewById<EditText>(R.id.fieldPassword).text.toString()
+            signIn(email, password)
         }
 
     }
+
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressDialog()
+
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            // .addOnCompleteListener(viewMain.context) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signInWithEmail:success")
+                val user = auth.currentUser
+                updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", task.exception)
+                Toast.makeText(
+                    viewMain.context, "Authentication failed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                updateUI(null)
+            }
+
+            // [START_EXCLUDE]
+            if (!task.isSuccessful) {
+                viewMain.findViewById<TextView>(R.id.status).setText(R.string.auth_failed)
+            }
+            hideProgressDialog()
+            // [END_EXCLUDE]
+        }
+        // [END sign_in_with_email]
+    }
+
+
+    // [START on_start_check_user]
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    // [END on_start_check_user]
+    private fun updateUI(user: FirebaseUser?) {
+
+        hideProgressDialog()
+        if (user != null) {
+            viewMain.findViewById<TextView>(R.id.status).text = getString(
+                R.string.emailpassword_status_fmt,
+                user.email, user.isEmailVerified
+            )
+            viewMain.findViewById<TextView>(R.id.detail).text = getString(R.string.firebase_status_fmt, user.uid)
+
+
+            viewMain.findViewById<LinearLayout>(R.id.emailPasswordButtons).visibility = View.GONE
+            viewMain.findViewById<LinearLayout>(R.id.emailPasswordFields).visibility = View.GONE
+            viewMain.findViewById<LinearLayout>(R.id.signedInButtons).visibility = View.VISIBLE
+
+            viewMain.findViewById<Button>(R.id.verifyEmailButton).isEnabled = !user.isEmailVerified
+        } else {
+            viewMain.findViewById<TextView>(R.id.status).setText(R.string.signed_out)
+            viewMain.findViewById<TextView>(R.id.detail).text = null
+
+            viewMain.findViewById<LinearLayout>(R.id.emailPasswordButtons).visibility = View.VISIBLE
+            viewMain.findViewById<LinearLayout>(R.id.emailPasswordFields).visibility = View.VISIBLE
+            viewMain.findViewById<LinearLayout>(R.id.signedInButtons).visibility = View.GONE
+        }
+    }
+
+    @VisibleForTesting
+    val progressDialog by lazy {
+        ProgressDialog(viewMain.context)
+    }
+
+    fun showProgressDialog() {
+        progressDialog.setMessage(getString(R.string.loading))
+        progressDialog.isIndeterminate = true
+        progressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        if (progressDialog.isShowing) {
+            progressDialog.dismiss()
+        }
+    }
+
+    fun hideKeyboard(view: View) {
+        val imm = viewMain.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        hideProgressDialog()
+    }
+
 
 }
